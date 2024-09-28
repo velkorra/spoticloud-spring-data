726890226f6d25d8a2cad83f578cc490b89abc25
vertunoff <anton.s.vertunov@gmail.com>
2024-09-28 13:50:37 +0300

    added constructor on username

--- src/main/java/com/example/spoticloudspringdata/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/spoticloudspringdata/exceptions/UserNotFoundException.java
@@ -1,7 +1,11 @@
 package com.example.spoticloudspringdata.exceptions;
 
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(int id){
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(int id) {
         super("User with id " + id + " not found");
+    }
+
+    public UserNotFoundException(String username) {
+        super("User with username " + username + " not found");
     }
 }
